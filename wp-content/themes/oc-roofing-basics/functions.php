364be2744a3270e482393c22642870538ff37526
Cale J Spencer <calejspencer@gmail.com>
2017-11-10 00:49:35 -0600

    Tweak asset queue scripts, remove p tags from content images

--- wp-content/themes/oc-roofing-basics/functions.php
+++ wp-content/themes/oc-roofing-basics/functions.php
@@ -1,46 +1,40 @@
 <?php // Custom Functions
 
-  /* Head Functions */
+  /* ----- Head Functions ------ */
 
   // Enqueue Styles and Scripts
-  if(!is_admin()) {
-    function enqueue_scripts() {
+    if(!is_admin()) {
       // Theme Stylesheet
-      wp_enqueue_style('theme_style', get_stylesheet_uri(), "", "2.1.4");
+      wp_enqueue_style('theme_style', get_stylesheet_uri(), "", "1.2");
       // Theme Scripts
-      wp_enqueue_script('theme_scripts', get_stylesheet_directory_uri() . '/assets/scripts/build.min.js', array('jquery'), "", true);
-    }
-    add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
-    // Remove query strings from static resources
-    function _remove_script_version( $src ){
-      $parts = explode( '?', $src );
-      return $parts[0];
-    }
-    add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
-    add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
-    // Remove jQuery Migrate
-    add_filter( 'wp_default_scripts', 'remove_jquery_migrate' );
-    function remove_jquery_migrate( &$scripts) {
-      if(!is_admin()) {
-        $scripts->remove( 'jquery');
-        $scripts->add( 'jquery', false, array( 'jquery-core' ), '3.2.1' );
+      wp_enqueue_script('theme_scripts', get_stylesheet_directory_uri() . '/assets/scripts/build.min.js', array('jquery'), false, true);
+      // wp_enqueue_script('sharethis', '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-582cd07f6a10f588', array(), '', true);
+
+      // Remove jQuery Migrate
+      add_filter( 'wp_default_scripts', 'remove_jquery_migrate' );
+      function remove_jquery_migrate( &$scripts) {
+        if(!is_admin()) {
+          $scripts->remove( 'jquery');
+          $scripts->add( 'jquery', false, array( 'jquery-core' ), '1.12.4' );
+        }
       }
-    }
-    // Defer Scripts
-    add_filter( 'script_loader_tag', 'wsds_defer_scripts', 10, 3 );
-    function wsds_defer_scripts( $tag, $handle, $src ) {
-      // The handles of the enqueued scripts we want to defer
-      $defer_scripts = array(
-        'theme_scripts',
-        'admin-bar',
-        'wp-embed'
-      );
-      if ( in_array( $handle, $defer_scripts ) ) {
-        return '<script src="' . $src . '" defer type="text/javascript"></script>' . "\n";
+
+      // Defer Scripts
+      add_filter( 'script_loader_tag', 'wsds_defer_scripts', 10, 3 );
+      function wsds_defer_scripts( $tag, $handle, $src ) {
+        // The handles of the enqueued scripts we want to defer
+        $defer_scripts = array(
+          'theme_scripts',
+          'admin-bar',
+          'wp-embed'
+        );
+        if ( in_array( $handle, $defer_scripts ) ) {
+            return '<script src="' . $src . '" defer="defer" type="text/javascript"></script>' . "\n";
+        }
+        return $tag;
       }
-      return $tag;
-    }
-  } // End if is not admin
+
+    } // End if is not admin
 
   // Render Page Title
   function render_site_title() {
@@ -68,6 +62,13 @@
 
   // Add Featured Images to Posts
   add_theme_support( 'post-thumbnails' );
+
+  // Remove p tags from content images
+  add_filter( 'the_content', 'remove_some_ptags' );
+  function remove_some_ptags( $content ) {
+    $content = preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+    return $content;
+  }
 
   // After Post Save Callback to Generate Static Pages
   add_action('transition_post_status', 'export_new_post', 10, 3);
